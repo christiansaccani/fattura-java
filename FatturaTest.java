58e4505a13b4bd3f35d3f77883dd5c7d5b2b96f3
Christian Saccani <138800179+christiansaccani@users.noreply.github.com>
2024-08-27 14:37:12 +0200

    Update FatturaTest.java

--- FatturaTest.java
+++ FatturaTest.java
@@ -3,21 +3,27 @@ package fattura;
 public class FatturaTest {
 
     public static void main(String[] args) {
-        // Creazione di un oggetto Intestatario
+        // oggetti Intestatario
         Intestatario i = new Intestatario("Leonardo", "Bianchi","N","SCCCRS99A26H223M");
         Intestatario i2 = new Intestatario("Pasquale", "Mazzocchi","N","SCCCRS926H223M");
         Intestatario i3 = new Intestatario("Rossi", "SRL","S","12345678901");
+
+        // oggetti Prodotto
+        Prodotto p = new Prodotto("Scarpe", 70.99);
+        Prodotto p2 = new Prodotto("Guanti", 5.99);
+        Prodotto p3 = new Prodotto("Sciarpa", 7.99);
         
-        
-        // Creazione di un oggetto Fattura
+        // oggetti Fattura
         Fattura f1 = new Fattura(i, "270824", 1456, "B");
         Fattura f3 = new Fattura(i2, "290824", 4298, "C");
         Fattura f2 = new Fattura(i2, "290824", -4298, "C");
         Fattura f4 = new Fattura(i, "280824", 5298, "Co");
         Fattura f5 = new Fattura(i3, "230624", 3201, "A");
+        Fattura f6 = new Fattura(i3, "230624", "A");
+        f6.aggiungiProdotto(p);
         
         // Stampa delle informazioni della fattura
-        System.out.println("f1: " + f1.toString());
+       System.out.println("f1: " + f1.toString());
         System.out.println("Numero univoco: " + Fattura.SequenzialeUnivoco.prossimoNumero());
         
         System.out.println("f2: " + f2.toString());
@@ -30,6 +36,9 @@ public class FatturaTest {
         System.out.println("Numero univoco: " + Fattura.SequenzialeUnivoco.prossimoNumero());
         
         System.out.println("f5: " + f5.toString());
+        System.out.println("Numero univoco: " + Fattura.SequenzialeUnivoco.prossimoNumero());
+        
+        System.out.println("f6: " + f6.toString());
         System.out.println("Numero univoco: " + Fattura.SequenzialeUnivoco.prossimoNumero());
     }
 }
