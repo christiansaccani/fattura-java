19a3ba60a39baa1aca7e8b023b0abebf5f6f4fe1
Christian Saccani <138800179+christiansaccani@users.noreply.github.com>
2024-08-27 14:39:13 +0200

    Update Fattura.java

--- Fattura.java
+++ Fattura.java
@@ -1,9 +1,11 @@
 package fattura;
+import java.util.ArrayList;
 
 public class Fattura {
 
     // Attributi privati della classe
     private Intestatario intestatario;
+    private ArrayList<Prodotto> prodotti;
     private String data;          
     private double ammontare;    
     private String modalita;  
@@ -12,6 +14,13 @@ public class Fattura {
         intestatario = new Intestatario(i.getNome(),i.getCognome(),i.getAzienda(),i.getIdentita());
         this.data = data;
         this.ammontare = ammontare;
+        this.modalita = modalita;
+    }
+    
+    public Fattura(Intestatario i, String data, String modalita) {
+        intestatario = new Intestatario(i.getNome(),i.getCognome(),i.getAzienda(),i.getIdentita());
+        this.prodotti = new ArrayList<Prodotto>();
+        this.data = data;
         this.modalita = modalita;
     }
     
@@ -25,16 +34,29 @@ public class Fattura {
         }
     }
     
+    public void aggiungiProdotto(Prodotto p) {
+        prodotti.add (p);
+    } 
+
+    public void calcolaTotale() {
+        double totale = 0;
+        for (Prodotto prodotto : prodotti) {
+            totale += prodotto.getCosto();
+        }
+        this.ammontare = Math.round(totale * 100.0) / 100.0;;
+  
+    }
+    
     public boolean isNegativo() {
-        return this.ammontare < 0;
+        return this.ammontare <= 0;
     }
     
     public boolean isNotOk() {
         return !modalita.equals("B") && !modalita.equals("C") && !modalita.equals("A");
     }
     
-    @Override
-    public String toString() {
+   
+    public String toStringA() {
         return "Fattura di: " + intestatario.getNome() + " "  + intestatario.getCognome() 
                 + " Data: " + data 
                 + " Ammontare: " + (isNegativo() ? "Non Valido" : "€ " + ammontare)
@@ -42,4 +64,14 @@ public class Fattura {
                 + " Identità: " + intestatario.isPiva();
     }
 
+    @Override
+    public String toString() {
+        try{
+              this.calcolaTotale();
+            return "Fattura: " + intestatario + " | prodotti:" + prodotti + " | data:" + data + " | ammontare:" + ammontare + " | modalita:" + modalita + '}';
+        }
+        catch(NullPointerException e) {
+            return "Fattura: " + intestatario + " | data:" + data + " | ammontare:" + ammontare + " | modalita:" + modalita + '}';
+        }
+    }
 }
